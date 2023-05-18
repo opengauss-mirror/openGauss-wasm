d55b155659173d4b5ff26b101230213a2b26e756
Nelson-He <heguofeng@huawei.com>
2023-05-18 17:41:55 +0800

    完善demo内容

--- wasm/examples/encrypt_decrypt/gen_wasm.sh
+++ wasm/examples/encrypt_decrypt/gen_wasm.sh
@@ -3,7 +3,7 @@
 set -e
 
 OPENGAUSS_EXPORTED_FUNC=$1
-OPENGAUSS_COMPILED_WASM=opengauss-target/wasm32-unknown-unknown/release/opengauss_encrypt_decrypt.wasm
+OPENGAUSS_COMPILED_WASM=opengauss-target/wasm32-unknown-unknown/release/encrypt_decrypt.wasm
 OPENGAUSS_OPTIMIZED_WASM=opengauss-target/encrypt_decrypt.wasm
 
 CARGO_TARGET_DIR=opengauss-target cargo build --release --target wasm32-unknown-unknown
